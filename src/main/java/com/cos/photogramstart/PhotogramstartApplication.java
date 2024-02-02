727f6a59e39805024506d825c294758300701bbe
getinthere <getinthere@naver.com>
2024-02-02 09:12:03 +0900

    포토그램 기본 세팅 완료

---
+++ src/main/java/com/cos/photogramstart/PhotogramstartApplication.java
@@ -0,0 +1,13 @@
+package com.cos.photogramstart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhotogramstartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhotogramstartApplication.class, args);
+	}
+
+}
